e605ba0442711b5b90ebfe0b137c64cf801bfe24
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-01-11 23:16:32 +0100

    Add set interface header

--- changelog.rst
+++ changelog.rst
@@ -1,3 +1,7 @@
+0.6.0
+=====
+- Add set interface header
+
 0.5.0
 =====
 - Add makefile rules to build the library
---
+++ include/set/set.h
@@ -0,0 +1,45 @@
+/**
+ * @file set.h
+ * @brief Set interface
+ */
+#ifndef SET_H
+#define SET_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+typedef void *set_t;
+typedef uint8_t set_element_t;
+typedef enum { DEFAULT_SET, BIT_VECTOR_SET } set_type_t;
+
+/**
+ * Initialize an empty set with a universe consisting of 0..n-1 integer elements.
+ * @return positive integer or 0 if success, negative one if fails
+ */
+int8_t set_init(set_t *set, uint8_t n, set_type_t type);
+
+/**
+ * Membership, insertion and deletion operations on a set `set`.
+ */
+bool set_member(set_t set, set_element_t e);
+int8_t set_insert(set_t set, set_element_t e);
+int8_t set_delete(set_t set, set_element_t e);
+void set_clear(set_t set);
+
+/**
+ * Perform UNION, INTERSECTION and DIFFERENCE set operations between
+ * `a` and `b` storing the result in an initialized set `c`. If `c` is
+ * not initialized, unexpected behavior might happen.
+ * @return positive integer or 0 if success, negative one if fails
+*/
+int8_t set_union(set_t a, set_t b, set_t *c);
+int8_t set_intersection(set_t a, set_t b, set_t *c);
+int8_t set_difference(set_t a, set_t b, set_t *c);
+
+/**
+ * Destroy the set `set`. It must be called when the set won't be used
+ * anymore.
+ */
+void set_destroy(set_t *set);
+
+#endif /* SET_H */
